b40100d8bfc1b4ce47853a9a17d9f2371aad8099
Dmytrenko1990 <dmitrenkoartem777@gmail.com>
2023-05-31 22:54:47 -0500

    Made minor changes

--- src/test/java/TestRunner/SmokeRunner.java
+++ src/test/java/TestRunner/SmokeRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "src/test/resources/Features",
         glue = "StepDefinitions",
         dryRun = false,
-        tags =  "@database",
+        tags =  "@smoke",
         //tags = "@testcase1 or @testcase2 and @smoke",
         plugin = {"pretty", "html:target/Cucumber.html", "json:target/Cucumber.json", "rerun:target/failed.txt"}
 )
